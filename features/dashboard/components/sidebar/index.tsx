27081391beb59c419cef6d5d653ef1fa38be7138
Dmitry <kfifa3@gmail.com>
2023-12-17 22:38:03 +0300

    feat: sidebar logout ui

--- features/dashboard/components/sidebar/index.tsx
+++ features/dashboard/components/sidebar/index.tsx
@@ -2,6 +2,7 @@ import SidebarUserInfo from "@/features/dashboard/components/sidebar/components/
 import { SIDEBAR_MENU_ITEMS } from "@/features/dashboard/components/sidebar/constants";
 import { useMemo } from "react";
 import SidebarMenuLink from "@/features/dashboard/components/sidebar/components/sidebar-menu-link";
+import { MdLogout } from "react-icons/md";
 
 const DashboardSideBar = () => {
 
@@ -22,6 +23,13 @@ const DashboardSideBar = () => {
     <div className='p-5 flex-1 bg-bg-light'>
       <SidebarUserInfo/>
       {menuItems}
+      <button
+        type='button'
+        className='w-full p-5 my-1.5 flex items-center gap-2.5 rounded-md hover:bg-bg-hover duration-500'
+      >
+        <MdLogout/>
+        Logout
+      </button>
     </div>
   );
 };
